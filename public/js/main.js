b13e1223a1dd63019692c5ec7c59095b7a9649a8
LukeCiar <lukemciarletta@gmail.com>
2026-09-07 17:28:30 -0400

    Integrated express and updated tab spacing

--- public/js/main.js
+++ public/js/main.js
@@ -5,128 +5,131 @@ let scoreTable = null
 //-1 = not modifying, index = modifying that index
 let modifying = -1
 
-const submit = async function(event) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
+const submit = async (event) => {
+    // stop form submission from trying to load
+    // a new .html page for displaying results...
+    // this was the original browser behavior and still
+    // remains to this day
+    event.preventDefault()
 
-  const userName = document.querySelector('#userName').value,
-        oppName = document.querySelector('#oppName').value,
-        userScore = parseInt(document.querySelector('#userScore').value) | 0,
-        oppScore = parseInt(document.querySelector('#oppScore').value) | 0,
-        json = { userName, oppName, userScore, oppScore }
+    const userName = document.querySelector('#userName').value,
+          oppName = document.querySelector('#oppName').value,
+          userScore = parseInt(document.querySelector('#userScore').value) | 0,
+          oppScore = parseInt(document.querySelector('#oppScore').value) | 0,
+          jsonData = { userName, oppName, userScore, oppScore }
 
-  let response
-  if(modifying === -1) {
-    //submtitting new data
-    response = await fetch('/submit', {
-      method:'POST',
-      body: JSON.stringify(json)
+    let response
+    if(modifying === -1) {
+        //submtitting new data
+        response = await fetch('/submit', {
+            method:'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(jsonData)
+        })
+    }
+    else {
+        //modifying existing data
+        jsonData.modifying = modifying
+        response = await fetch('/modify', {
+            method:'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(jsonData)
+        })
+        setModifying(modifying) //reset to no longer be modifying
+    }
+
+    const allGames = await response.json()
+    updateScoreTable(allGames)
+}
+
+const deleteGame = async (i) => {
+    //remove currently modifying if is the one being deleted - breaks modifying feature otherwise
+    if(modifying === i) { setModifying(i) }
+
+    const response = await fetch('/delete', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ deleteIndex: i })
     })
-  }
-  else {
-    //modifying existing data
-    json.modifying = modifying
-    response = await fetch('/modify', {
-      method:'POST',
-      body: JSON.stringify(json)
-    })
-    setModifying(modifying) //reset to no longer be modifying
-  }
 
-  const allGames = await response.json()
-  updateScoreTable(allGames)
+    const allGames = await response.json()
+    updateScoreTable(allGames)
 }
 
-const deleteGame = async function(i) {
-  //remove currently modifying in case it is the one being deleted - breaks modifying feature otherwise
-  setModifying(modifying)
+const setModifying = (i) => {
+    if(modifying != -1 && modifying != i) {
+        //reset the currently modifying game (needed for button highlight)
+        setModifying(modifying)
+    }
+    
+    //used to highlight the relevant modify button
+    const button = scoreTable.children[i+1].children[6]
+    const modifyInstructions = document.querySelector("#modifyInstructions")
 
-  const response = await fetch('/delete', {
-    method: 'POST',
-    body: i
-  })
-
-  const allGames = await response.json()
-  updateScoreTable(allGames)
+    if(modifying === i) {
+        modifying = -1;
+        button.className = ""
+        modifyInstructions.className = "hidden"
+    }
+    else {
+        modifying = i
+        button.className = "modifying"
+        modifyInstructions.className = ""
+    }
 }
 
-const setModifying = function(i) {
-  if(modifying != -1 && modifying != i) {
-    //reset the currently modifying game (needed for button highlight)
-    setModifying(modifying)
-  }
-  
-  //used to highlight the relevant modify button
-  const button = scoreTable.children[i+1].children[6]
-  const modifyInstructions = document.querySelector("#modifyInstructions")
+const updateScoreTable = (allGames) => {
+    scoreTable.innerHTML = `<tr>
+        <th>Your Name</th>
+        <th>Opponent's Name</th>
+        <th>Your Score</th>
+        <th>Opponent's Score</th>
+        <th>Result</th>
+        <th>Del.</th>
+        <th>Mod.</th>
+    </tr>` //last two headers are for delete and modify buttons
 
-  if(modifying === i) {
-    modifying = -1;
-    button.className = ""
-    modifyInstructions.className = "hidden"
-  }
-  else {
-    modifying = i
-    button.className = "modifying"
-    modifyInstructions.className = ""
-  }
+    for (let i = 0; i < allGames.length; i++) {
+        const deleteButton = document.createElement('button')
+        deleteButton.innerHTML = '<img class="icon" src="../icons/delete.png" />'
+        deleteButton.onclick = async () => await deleteGame(i)
+        const deleteButtonCell = document.createElement('td')
+        deleteButtonCell.appendChild(deleteButton)
+
+        const modifyButton = document.createElement('button')
+        modifyButton.innerHTML = '<img class="icon" src="../icons/modify.png" />'
+        modifyButton.onclick = () => setModifying(i)
+        const modifyButtonCell = document.createElement('td')
+        modifyButtonCell.appendChild(modifyButton)
+
+        game = allGames[i]
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <td>${game.userName}</td>
+        <td>${game.oppName}</td>
+        <td>${game.userScore}</td>
+        <td>${game.oppScore}</td>
+        <td>${game.result}</td>
+        `
+        tr.appendChild(deleteButtonCell)
+        tr.appendChild(modifyButtonCell)
+        scoreTable.appendChild(tr)
+
+        document.querySelector('form').reset() //clear form
+    }
 }
 
-const updateScoreTable = function(allGames) {
-  scoreTable.innerHTML = `<tr>
-    <th>Your Name</th>
-    <th>Opponent's Name</th>
-    <th>Your Score</th>
-    <th>Opponent's Score</th>
-    <th>Result</th>
-    <th>Del.</th>
-    <th>Mod.</th>
-  </tr>` //last two headers are for delete and modify buttons
+window.onload = () => {
+    //add submit function to the form instead of the button
+        //so that default input validation works
+    document.querySelector('form').onsubmit = submit
 
-  for (let i = 0; i < allGames.length; i++) {
-    const deleteButton = document.createElement('button')
-    deleteButton.innerHTML = '<img class="icon" src="../icons/delete.png" />'
-    deleteButton.onclick = async () => await deleteGame(i)
-    const deleteButtonCell = document.createElement('td')
-    deleteButtonCell.appendChild(deleteButton)
+    scoreTable = document.createElement('table')
+    document.body.append(scoreTable)
 
-    const modifyButton = document.createElement('button')
-    modifyButton.innerHTML = '<img class="icon" src="../icons/modify.png" />'
-    modifyButton.onclick = () => setModifying(i)
-    const modifyButtonCell = document.createElement('td')
-    modifyButtonCell.appendChild(modifyButton)
-
-    game = allGames[i]
-    const tr = document.createElement('tr')
-    tr.innerHTML = `
-      <td>${game.userName}</td>
-      <td>${game.oppName}</td>
-      <td>${game.userScore}</td>
-      <td>${game.oppScore}</td>
-      <td>${game.result}</td>
-    `
-    tr.appendChild(deleteButtonCell)
-    tr.appendChild(modifyButtonCell)
-    scoreTable.appendChild(tr)
-
-    document.querySelector('form').reset() //clear form
-  }
-}
-
-window.onload = function() {
-  //add submit function to the form instead of the button
-    //so that default input validation works
-  document.querySelector('form').onsubmit = submit
-
-  scoreTable = document.createElement('table')
-  document.body.append(scoreTable)
-
-  let modifyText = document.createElement('p')
-  modifyText.innerText = "To modify this game, enter the new details into the form and press submit."
-  modifyText.className = "hidden"
-  modifyText.id = "modifyInstructions"
-  document.body.append(modifyText)
+    let modifyText = document.createElement('p')
+    modifyText.innerText = "To modify this game, enter the new details into the form and press submit."
+    modifyText.className = "hidden"
+    modifyText.id = "modifyInstructions"
+    document.body.append(modifyText)
 }
