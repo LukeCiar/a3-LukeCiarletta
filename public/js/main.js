cbbf188855afbb31a9c1620cd37be6cc616dd6d9
LukeCiar <lukemciarletta@gmail.com>
2026-09-08 15:10:58 -0400

    Integrate MongoDB

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,9 @@
 
 let scoreTable = null
 
-//-1 = not modifying, index = modifying that index
-let modifying = -1
+//-1 = not modifying, number = modifying that index/id
+let modifyingRow = -1
+let modifyingId = -1
 
 const submit = async (event) => {
     // stop form submission from trying to load
@@ -19,7 +20,7 @@ const submit = async (event) => {
           jsonData = { userName, oppName, userScore, oppScore }
 
     let response
-    if(modifying === -1) {
+    if(modifyingRow === -1) {
         //submtitting new data
         response = await fetch('/submit', {
             method:'POST',
@@ -29,50 +30,56 @@ const submit = async (event) => {
     }
     else {
         //modifying existing data
-        jsonData.modifying = modifying
+        jsonData.modifyingId = modifyingId
         response = await fetch('/modify', {
             method:'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(jsonData)
         })
-        setModifying(modifying) //reset to no longer be modifying
+        clearModifying()
     }
 
     const allGames = await response.json()
     updateScoreTable(allGames)
 }
 
-const deleteGame = async (i) => {
-    //remove currently modifying if is the one being deleted - breaks modifying feature otherwise
-    if(modifying === i) { setModifying(i) }
+const deleteGame = async (id) => {
+    //remove currently modifying row in case it is the one being deleted - otherwise modifying would break
+    clearModifying()
 
     const response = await fetch('/delete', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ deleteIndex: i })
+        body: JSON.stringify({id})
     })
 
     const allGames = await response.json()
     updateScoreTable(allGames)
 }
 
-const setModifying = (i) => {
-    if(modifying != -1 && modifying != i) {
-        //reset the currently modifying game (needed for button highlight)
-        setModifying(modifying)
+const clearModifying = () => { 
+    if(modifyingRow != -1) { setModifying(modifyingRow, -1) }
+}
+
+const setModifying = (row, id) => {
+    if(modifyingRow != -1 && modifyingRow != row) {
+        //reset the currently modifying game (needed to avoid 2 button highlights)
+        clearModifying()
     }
     
     //used to highlight the relevant modify button
-    const button = scoreTable.children[i+1].children[6]
+    const button = scoreTable.children[row+1].children[6]
     const modifyInstructions = document.querySelector("#modifyInstructions")
 
-    if(modifying === i) {
-        modifying = -1;
+    if(modifyingRow === row) {
+        modifyingRow = -1
+        modifyingId = -1
         button.className = ""
         modifyInstructions.className = "hidden"
     }
     else {
-        modifying = i
+        modifyingRow = row
+        modifyingId = id
         button.className = "modifying"
         modifyInstructions.className = ""
     }
@@ -90,19 +97,20 @@ const updateScoreTable = (allGames) => {
     </tr>` //last two headers are for delete and modify buttons
 
     for (let i = 0; i < allGames.length; i++) {
+        let game = allGames[i]
+
         const deleteButton = document.createElement('button')
         deleteButton.innerHTML = '<img class="icon" src="../icons/delete.png" />'
-        deleteButton.onclick = async () => await deleteGame(i)
+        deleteButton.onclick = async () => { await deleteGame(game._id) }
         const deleteButtonCell = document.createElement('td')
         deleteButtonCell.appendChild(deleteButton)
 
         const modifyButton = document.createElement('button')
         modifyButton.innerHTML = '<img class="icon" src="../icons/modify.png" />'
-        modifyButton.onclick = () => setModifying(i)
+        modifyButton.onclick = () => { setModifying(i, game._id) }
         const modifyButtonCell = document.createElement('td')
         modifyButtonCell.appendChild(modifyButton)
 
-        game = allGames[i]
         const tr = document.createElement('tr')
         tr.innerHTML = `
         <td>${game.userName}</td>
@@ -114,12 +122,12 @@ const updateScoreTable = (allGames) => {
         tr.appendChild(deleteButtonCell)
         tr.appendChild(modifyButtonCell)
         scoreTable.appendChild(tr)
-
-        document.querySelector('form').reset() //clear form
     }
+
+    document.querySelector('form').reset() //clear form
 }
 
-window.onload = () => {
+window.onload = async () => {
     //add submit function to the form instead of the button
         //so that default input validation works
     document.querySelector('form').onsubmit = submit
@@ -132,4 +140,8 @@ window.onload = () => {
     modifyText.className = "hidden"
     modifyText.id = "modifyInstructions"
     document.body.append(modifyText)
+
+    const response = await fetch('/games', { method: 'GET' })
+    const allGames = await response.json()
+    updateScoreTable(allGames)
 }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express'),
       app = express(),
+      { MongoClient, ObjectId } = require("mongodb"),
       port = process.env.PORT || 3000
 
 const appData = []
@@ -7,40 +9,68 @@ const appData = []
 app.use(express.static('public'))
 app.use(express.json())
 
-const sendData = (req, res) => {
-    res.status(200).send(appData)
-}
-
-app.post('/submit', (req, res) => {
-    data = req.body
-    setResult(data)
-    appData.push(data)
-    sendData(req, res)
-})
-
-app.post('/delete', (req, res) => {
-    index = req.body.deleteIndex
-    appData.splice(index, 1)
-    sendData(req, res)
-})
-
-app.post('/modify', (req, res) => {
-    data = req.body
-    setResult(data)
-
-    //pull out the index to modify and prevent it from getting saved
-    index = data.modifying
-    delete data.modifying
-
-    appData.splice(index, 1, data)
-    sendData(res, res)
-})
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri)
+let collection //will be set by middleware on first request
 
 const setResult = (data) => {
     if(data.userScore > data.oppScore) { data.result = "Win" }
     else if(data.userScore < data.oppScore) { data.result = "Loss" }
     else { data.result = "Tie" }
 }
+
+const sendData = async (req, res) => {
+    allGames = await getGames()
+    res.status(200).send(allGames)
+}
+
+const getGames = async () => {
+    const games = await collection.find().toArray()
+    return games
+}
+
+const addGame = async (game) => {
+    const id = await collection.insertOne(game)
+}
+
+const deleteGame = async (deleteId) => {
+    const result = await collection.deleteOne({_id: new ObjectId(deleteId)})
+}
+
+const modifyGame = async (game) => {
+    modifyingId = game.modifyingId
+    delete game.modifyingId //prevent it from getting pushed to db
+    const query = { _id: new ObjectId(modifyingId) }
+    const result = await collection.replaceOne(query, game)
+}
+
+//make sure that collection is set on first request
+app.use(async (req, res, next) => {
+    if (!collection) { collection = await client.db("a3").collection("game-data") }
+    next()
+})
+
+app.get('/games', sendData)
+
+app.post('/submit', async (req, res) => {
+    game = req.body
+    setResult(game)
+    await addGame(game)
+    await sendData(req, res)
+})
+
+app.post('/delete', async (req, res) => {
+    deleteId = req.body.id
+    await deleteGame(deleteId)
+    await sendData(req, res)
+})
+
+app.post('/modify', async (req, res) => {
+    game = req.body
+    setResult(game)
+    await modifyGame(game)
+    await sendData(res, res)
+})
 
 app.listen(port)
 console.log("Listening on port " + port)
